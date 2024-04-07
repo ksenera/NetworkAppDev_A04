f92a07ed9069fbe81033f8d42c5043cb68a08e3f
ksenera <ksenera7130@conestogac.on.ca>
2024-04-07 17:42:40 -0400

    added ajax call

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -7,3 +8,5 @@ def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world'})
