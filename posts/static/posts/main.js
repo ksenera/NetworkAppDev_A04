4171e9ae8d13fca137c686b089ef74265c8036d0
ksenera <ksenera7130@conestogac.on.ca>
2024-04-07 19:18:55 -0400

    added crsf token

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -5,6 +5,48 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
+const getCookie =(name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = ()=> {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=>{
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "", 
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response){
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    }))
+
+}
+
 let visible = 3 
 
 const getData = () => {
@@ -30,7 +72,10 @@ const getData = () => {
                                         <a href="#" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">${element.liked ? `Unlike (${element.count})` : `Like (${element.count})`}</a>
+                                        <form class="like-unlike-forms" data-form-id="${element.id}">
+                                            {% csrf_token %}
+                                            <button href="#" class="btn btn-primary" id="like-unlike-${element.id}>${element.liked ? `Unlike (${element.count})` : `Like (${element.count})`}</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
