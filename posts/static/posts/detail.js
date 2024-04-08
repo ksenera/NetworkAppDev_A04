fbaace3617981387ad5ffe3be13f54a8656786f1
ksenera <ksenera7130@conestogac.on.ca>
2024-04-07 21:46:09 -0400

    added back button event listener

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+console.log('hello world detail')
+
+
+const backBtn = document.getElementById('back-btn')
+
+
+backBtn.addEventListener('click', ()=>{
+    history.back()
+})
